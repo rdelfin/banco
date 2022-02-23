9ac76798012185d67edbdcfbb065e8a765cc1143
Ricardo Delfin <me@rdelfin.com>
2022-02-23 21:02:45 +0000

    Added tarpc and interfaces

--- banco_interfaces/build.rs
+++
@@ -1,5 +0,0 @@
-use std::io::Result;
-fn main() -> Result<()> {
-    prost_build::compile_protos(&["protos/interface.proto"], &[])?;
-    Ok(())
-}
--- banco_interfaces/src/lib.rs
+++ banco_interfaces/src/lib.rs
@@ -1,1 +1,47 @@
-tonic::include_proto!("banco");
+//! This module exposes all the interfaces used by banco, both from nodes, and teller. Use these to
+//! communicate to other components over IPC.
+
+use std::collections::HashMap;
+
+/// This interface is used to talk to Teller, the broker and manager for all nodes and topics.
+#[tarpc::service]
+pub trait Teller {
+    /// List all registered nodes.
+    async fn list_nodes() -> ListNodesResponse;
+    /// Request teller to start a node. It will create a process, and return once the `fork`
+    /// succeeeded.
+    async fn start_node(node: Node);
+
+    /// Endpoint used for the nodes to heartbeat back to teller.
+    async fn heartbeat(node_name: String);
+}
+
+/// Response to listing of all nodes
+#[derive(Debug, Clone)]
+pub struct ListNodesResponse {
+    /// Hashmap of said nodes
+    pub nodes: HashMap<String, Node>,
+}
+
+/// A representation of the current state of a node from the view of Teller
+#[derive(Debug, Clone)]
+pub struct Node {
+    /// The registered name of the node. It will remain consistent throughout a run.
+    pub name: String,
+    /// The path to the executable used for the binary.
+    pub executable_path: String,
+    /// The status the node is in (e.g. running, stopped).
+    pub status: NodeStatus,
+}
+
+/// Enum representing the current state of a node.
+#[derive(Debug, Clone)]
+pub enum NodeStatus {
+    /// Used once the node has been created, it is identified as alive, but hasn't finished its
+    /// internally-defined initialisation.
+    INITIALISING,
+    /// Used once the node is running and in a good state
+    RUNNING,
+    /// Used for when a node is no longer running, be it intentionally or after a crash.
+    STOPPED { crashed: bool },
+}
